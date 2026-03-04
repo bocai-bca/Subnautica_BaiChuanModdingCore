b108a8aa80b1e7923e2d12a9488a34c9bdb984d4
GAZZZAMYN <3263733451@qq.com>
2026-03-05 04:07:14 +0800

    优化一个描述问题

--- src/BaiChuanModdingCore/Patch_Language_TryGet.cs
+++ src/BaiChuanModdingCore/Patch_Language_TryGet.cs
@@ -8,7 +8,7 @@ namespace BaiChuanModdingCore
 	{
 		public static Dictionary<string, string> OverrideTranslations = new()
 		{
-			{"Tooltip_StasisRifle", "通过改变磁场使物体在一段时间内处于停滞状态\n<color=#FF3030FF>射击遗迹和建筑会解除其静止状态</color>"},
+			{"Tooltip_StasisRifle", "通过改变磁场使物体在一段时间内处于停滞状态\n<color=#FF3030FF>射击遗迹和残骸会解除其静止状态</color>"},
 			{"Tooltip_Battery", "便携能源\n<color=#FFA500>制作需要电池或含有电池的工具需要保持满电</color>"},
 			{"Tooltip_SeamothTorpedoModule", "一个标准的水下发射装备，可以用于发射鱼雷\n<color=#FFA500>该模块只能装备在一到四号槽位</color>"},
 			{"Tooltip_VehicleStorageModule", "额外增加的4x4储存空间，适用于海蛾号和海虾号动力机甲\n<color=#FFA500>该模块只能装备在一到四号槽位</color>"},
