1bd9ff5625069db59ad406dfd08049f4495d3250
Bocai_BCA <105545253+bocai-bca@users.noreply.github.com>
2026-03-04 16:30:11 +0800

    增加原版翻译键拦截补丁

---
+++ src/BaiChuanModdingCore/Patch_Language_TryGet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace BaiChuanModdingCore
+{
+	[HarmonyPatch(typeof(Language), nameof(Language.TryGet))]
+	public class Patch_Language_TryGet
+	{
+		public static Dictionary<string, string> OverrideTranslations = new()
+		{
+			{"Quartz", "石英(翻译覆写测试)"},
+		};
+
+		public static void Prefix(string key, out string __state)
+		{
+			__state = key;
+		}
+
+		public static void Postfix(ref string result, string __state)
+		{
+			if (OverrideTranslations.TryGetValue(__state, out string? translationOverrided))
+			{
+				result = translationOverrided;
+			}
+		}
+	}
+}
