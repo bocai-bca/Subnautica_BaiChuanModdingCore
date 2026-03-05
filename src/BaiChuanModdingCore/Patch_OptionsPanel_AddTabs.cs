b8c0c060c592d259d7354626960082bf517ea81f
Bocai_BCA <105545253+bocai-bca@users.noreply.github.com>
2026-03-06 01:04:56 +0800

    再次变更Mods标签页的拦截方式

--- src/BaiChuanModdingCore/Patch_OptionsPanel_AddTabs.cs
+++ src/BaiChuanModdingCore/Patch_OptionsPanel_AddTabs.cs
@@ -1,11 +1,13 @@
 ﻿using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BaiChuanModdingCore
 {
 	[HarmonyPatch(typeof(uGUI_OptionsPanel), "AddTabs")]
 	public class Patch_OptionsPanel_AddTabs
 	{
+		/*
 		public static void Postfix(uGUI_OptionsPanel __instance)
 		{
 			for (int i = 0; i < __instance.tabs.Count; i++)
@@ -17,6 +19,21 @@ namespace BaiChuanModdingCore
 				__instance.tabs.RemoveAt(i);
 				BaiChuanModdingCore.logger?.LogMessage("Successed to remove Mods tab from OptionsPanel.");
 				return;
+			}
+			BaiChuanModdingCore.logger?.LogWarning("Failed to find Mods tab from OptionsPanel.");
+		}*/
+
+		public static void Postfix(uGUI_OptionsPanel __instance)
+		{
+			Transform tabsHolder = __instance.transform.Find("TabsHolder");
+			for (int i = 0; i < tabsHolder.childCount; i++)
+			{
+				Transform tab = tabsHolder.GetChild(i);
+				Text text = tab.GetComponentInChildren<Text>();
+				if (text == null) continue;
+				if (text.text != "Mods") continue;
+				tab.gameObject.SetActive(false);
+				BaiChuanModdingCore.logger?.LogMessage("Successed to hide Mods tab from OptionsPanel.");
 			}
 			BaiChuanModdingCore.logger?.LogWarning("Failed to find Mods tab from OptionsPanel.");
 		}
