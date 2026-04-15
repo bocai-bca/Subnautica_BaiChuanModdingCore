c931541fa9825dba523450ca60cae33878efb0a5
Bocai_BCA <105545253+bocai-bca@users.noreply.github.com>
2026-04-15 22:16:19 +0800

    再次重写生物修补方式

--- src/BaiChuanModdingCore/BaiChuanModdingCore.cs
+++ src/BaiChuanModdingCore/BaiChuanModdingCore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Logging;
@@ -5,7 +6,7 @@ using HarmonyLib;
 
 namespace BaiChuanModdingCore
 {
-	[BepInPlugin("net.bcasoft.baichuanmoddingcore", "BaiChuanModdingCore", "0.1.2.0")]
+	[BepInPlugin("net.bcasoft.baichuanmoddingcore", "BaiChuanModdingCore", "0.1.3.0")]
 	public class BaiChuanModdingCore : BaseUnityPlugin
 	{
 		internal Harmony harmony = new Harmony("net.bcasoft.baichuanmoddingcore");
@@ -13,13 +14,22 @@ namespace BaiChuanModdingCore
 		private void Awake()
 		{
 			Instance = this;
-			HarmonyMethod prefix = new HarmonyMethod(typeof(Patch_CraftData).GetMethod("Prefix", BindingFlags.Static | BindingFlags.Public));
-			harmony.Patch(typeof(CraftData).GetMethod("GetPrefabForTechType", BindingFlags.Public | BindingFlags.Static), prefix,new HarmonyMethod(typeof(Patch_CraftData).GetMethod("Postfix", BindingFlags.Static | BindingFlags.Public)));
-			harmony.Patch(typeof(CraftData).GetMethod("GetPrefabForTechTypeAsync", BindingFlags.Public | BindingFlags.Static, null, new []{typeof(TechType), typeof(bool)}, null), prefix,new HarmonyMethod(typeof(Patch_CraftData).GetMethod("PostfixAsync", BindingFlags.Static | BindingFlags.Public)));
-			harmony.Patch(typeof(MainMenuMusic).GetMethod("Play", BindingFlags.Public | BindingFlags.Static), null, new HarmonyMethod(typeof(MainMenuMusicModded).GetMethod("PlayPostfix", BindingFlags.Static | BindingFlags.Public)));
-			harmony.Patch(typeof(MainMenuMusic).GetMethod("Stop", BindingFlags.Public | BindingFlags.Static), null, new HarmonyMethod(typeof(MainMenuMusicModded).GetMethod("StopPostfix", BindingFlags.Static | BindingFlags.Public)));
-			harmony.PatchAll();
 			logger = Logger;
+			try
+			{
+				HarmonyMethod prefix = new HarmonyMethod(typeof(Patch_CraftData).GetMethod("Prefix", BindingFlags.Static | BindingFlags.Public));
+				harmony.Patch(typeof(CraftData).GetMethod("GetPrefabForTechType", BindingFlags.Public | BindingFlags.Static), prefix,new HarmonyMethod(typeof(Patch_CraftData).GetMethod("Postfix", BindingFlags.Static | BindingFlags.Public)));
+				harmony.Patch(typeof(CraftData).GetMethod("GetPrefabForTechTypeAsync", BindingFlags.Public | BindingFlags.Static, null, [typeof(TechType), typeof(bool)], null), prefix,new HarmonyMethod(typeof(Patch_CraftData).GetMethod("PostfixAsync", BindingFlags.Static | BindingFlags.Public)));
+				harmony.Patch(typeof(MainMenuMusic).GetMethod("Play", BindingFlags.Public | BindingFlags.Static), null, new HarmonyMethod(typeof(MainMenuMusicModded).GetMethod("PlayPostfix", BindingFlags.Static | BindingFlags.Public)));
+				harmony.Patch(typeof(MainMenuMusic).GetMethod("Stop", BindingFlags.Public | BindingFlags.Static), null, new HarmonyMethod(typeof(MainMenuMusicModded).GetMethod("StopPostfix", BindingFlags.Static | BindingFlags.Public)));
+				CreaturePatches.DoPatch(harmony);
+				harmony.PatchAll();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
 			if (!MainMenuMusicModded.LoadModMusic())
 			{
 				logger?.LogError("Failed to load mod music.");
---
+++ src/BaiChuanModdingCore/CreaturePatches.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BaiChuanModdingCore
+{
+	/// <summary>
+	/// 生物补丁，此类容纳一堆方法用于修补原版生物这种会在世界生成时预装非prefab提供的数据的傻逼玩意
+	/// </summary>
+	public class CreaturePatches
+	{
+		/// <summary>
+		/// 修补任务列表，键为对应生物类型，值为对应生物的Start方法的Postfix方法
+		/// </summary>
+		public static Dictionary<Type, string[]> StartTasks = new()
+		{
+			{ typeof(Creature), [string.Empty, nameof(Creature_Start_Postfix)] },
+			//{ typeof(GhostLeviathan), [string.Empty, nameof(GhostLeviathan_Start_Postfix)] },
+			//{ typeof(Reefback), [string.Empty, nameof(Reefback_Start_Postfix)] },
+			//{ typeof(SeaDragon), [string.Empty, nameof(SeaDragon_Start_Postfix)] },
+			//{ typeof(Warper), [string.Empty, nameof(Warper_Start_Postfix)] },
+			//{ typeof(ReaperLeviathan), [string.Empty, nameof(ReaperLeviathan_Start_Postfix)] },
+			//{ typeof(SeaEmperorJuvenile), [string.Empty, nameof(SeaEmperorJuvenile_Start_Postfix)] },
+		};
+		
+		/// <summary>
+		/// 打入补丁，本方法自身不带异常捕捉
+		/// </summary>
+		/// <param name="harmony">修补用的harmony实例</param>
+		public static void DoPatch(Harmony harmony)
+		{
+			BaiChuanModdingCore.logger?.LogMessage("Starting CreaturePatches patching.");
+			foreach (KeyValuePair<Type, string[]> startPostfixTask in StartTasks)
+			{
+				MethodBase? originalMethod = startPostfixTask.Key.GetMethod("Start", BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic); 
+				HarmonyMethod? prefixMethod = startPostfixTask.Value[0] == string.Empty ? null : new HarmonyMethod(typeof(CreaturePatches).GetMethod(startPostfixTask.Value[0], BindingFlags.Static | BindingFlags.Public));
+				HarmonyMethod? postfixMethod = startPostfixTask.Value[1] == string.Empty ? null : new HarmonyMethod(typeof(CreaturePatches).GetMethod(startPostfixTask.Value[1], BindingFlags.Static | BindingFlags.Public));
+				if (originalMethod == null)
+				{
+					BaiChuanModdingCore.logger?.LogError("Failed to patch the type " + startPostfixTask.Key.Name + " of CreaturePatches, because it was failed to get original method.");
+					continue;
+				}
+				harmony.Patch(
+					original: originalMethod,
+					prefix: prefixMethod,
+					postfix: postfixMethod
+				);
+				BaiChuanModdingCore.logger?.LogMessage("The type " + startPostfixTask.Key.Name + " of CreaturePatches was patched.");
+			}
+			BaiChuanModdingCore.logger?.LogMessage("CreaturePatches patching end.");
+		}
+
+		public static void Creature_Start_Postfix(Creature __instance)
+		{
+			switch (__instance)
+			{
+				case GhostLeviathan ghostLeviathan:
+					GhostLeviathan_Start_Postfix(ghostLeviathan);
+					break;
+				case ReaperLeviathan reaperLeviathan:
+					ReaperLeviathan_Start_Postfix(reaperLeviathan);
+					break;
+				case SeaDragon seaDragon:
+					SeaDragon_Start_Postfix(seaDragon);
+					break;
+				case Warper warper:
+					Warper_Start_Postfix(warper);
+					break;
+				case Reefback reefback:
+					Reefback_Start_Postfix(reefback);
+					break;
+				case SeaEmperorJuvenile seaEmperorJuvenile:
+					SeaEmperorJuvenile_Start_Postfix(seaEmperorJuvenile);
+					break;
+			}
+		}
+		
+		public static void GhostLeviathan_Start_Postfix(GhostLeviathan __instance)
+		{
+			LiveMixin liveMixin = __instance.GetComponent<LiveMixin>();
+			if (liveMixin == null)
+			{
+				return;
+			}
+			// 检查如果最大血量为360000或315000则跳过修补
+			if (Mathf.Approximately(liveMixin.data.maxHealth, 360000f) || Mathf.Approximately(liveMixin.data.maxHealth, 315000f))
+			{
+				return;
+			}
+			// 将血量改为360000
+			liveMixin.data.maxHealth = liveMixin.health = 360000f;
+			// 目前不知道怎么甄别未修补的成年幽灵和青年幽灵，所以索性将所有未修补的幽灵都改为成年血量了
+		}
+		
+		public static void Reefback_Start_Postfix(Reefback __instance)
+		{
+			LiveMixin liveMixin = __instance.GetComponent<LiveMixin>();
+			if (liveMixin == null)
+			{
+				return;
+			}
+			if (Mathf.Approximately(liveMixin.data.maxHealth, 450000f))
+			{
+				return;
+			}
+			liveMixin.data.maxHealth = liveMixin.health = 450000f;
+		}
+		
+		public static void ReaperLeviathan_Start_Postfix(ReaperLeviathan __instance)
+		{
+			LiveMixin liveMixin = __instance.GetComponent<LiveMixin>();
+			if (liveMixin == null)
+			{
+				return;
+			}
+			if (Mathf.Approximately(liveMixin.data.maxHealth, 225000f))
+			{
+				return;
+			}
+			liveMixin.data.maxHealth = liveMixin.health = 225000f;
+		}
+		
+		public static void SeaDragon_Start_Postfix(SeaDragon __instance)
+		{
+			LiveMixin liveMixin = __instance.GetComponent<LiveMixin>();
+			if (liveMixin == null)
+			{
+				return;
+			}
+			if (Mathf.Approximately(liveMixin.data.maxHealth, 360000f))
+			{
+				return;
+			}
+			liveMixin.data.maxHealth = liveMixin.health = 360000f;
+		}
+		
+		public static void SeaEmperorJuvenile_Start_Postfix(SeaEmperorJuvenile __instance)
+		{
+			LiveMixin liveMixin = __instance.GetComponent<LiveMixin>();
+			if (liveMixin == null)
+			{
+				return;
+			}
+			if (Mathf.Approximately(liveMixin.data.maxHealth, 225000f))
+			{
+				return;
+			}
+			liveMixin.data.maxHealth = liveMixin.health = 225000f;
+		}
+		
+		public static void Warper_Start_Postfix(Warper __instance)
+		{
+			LiveMixin liveMixin = __instance.GetComponent<LiveMixin>();
+			if (liveMixin == null)
+			{
+				return;
+			}
+			if (Mathf.Approximately(liveMixin.data.maxHealth, 8100f))
+			{
+				return;
+			}
+			liveMixin.data.maxHealth = liveMixin.health = 8100f;
+		}
+	}
+}
