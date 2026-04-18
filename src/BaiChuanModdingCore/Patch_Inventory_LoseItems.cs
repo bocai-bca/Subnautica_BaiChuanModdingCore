1ed9f3ea18548046d5f9a4e9c6adbe09aca09669
Bocai_BCA <105545253+bocai-bca@users.noreply.github.com>
2026-04-18 09:51:21 +0800

    添加模拟物品掉落的功能，检测不在载具内、驾驶时触发

--- src/BaiChuanModdingCore/Patch_Inventory_LoseItems.cs
+++ src/BaiChuanModdingCore/Patch_Inventory_LoseItems.cs
@@ -1,9 +1,10 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace BaiChuanModdingCore;
 
 /// <summary>
-/// 物品掉落补丁，将在死前使所有物品变为可掉落状态
+/// 物品掉落补丁，将在死前使所有物品变为可掉落状态，在游戏原生掉落物品后再强制执行掉落物品
 /// </summary>
 [HarmonyPatch(typeof(Inventory), nameof(Inventory.LoseItems))]
 public class Patch_Inventory_LoseItems
@@ -12,6 +13,7 @@ public class Patch_Inventory_LoseItems
 	{
 		if (BaiChuanModdingCore.dropAllOnDeath == null) return;
 		if (!BaiChuanModdingCore.dropAllOnDeath.Value) return;
+		BaiChuanModdingCore.logger?.LogMessage("De-secure inventory items.");
 		foreach (InventoryItem item in Inventory.main.container)
 		{
 			item.item.destroyOnDeath = true;
@@ -19,6 +21,28 @@ public class Patch_Inventory_LoseItems
 		foreach (InventoryItem item in (IItemsContainer)Inventory.main.equipment)
 		{
 			item.item.destroyOnDeath = true;
+		}
+	}
+
+	public static void Postfix(Inventory __instance)
+	{
+		if (BaiChuanModdingCore.dropAllOnDeath == null) return;
+		if (!BaiChuanModdingCore.dropAllOnDeath.Value) return;
+		Player player = Player.main;
+		if (player.currentSub != null || player.isPiloting) return;
+		// 模拟丢物品
+		BaiChuanModdingCore.logger?.LogMessage("Simulating death drop.");
+		foreach (InventoryItem item in Inventory.main.container)
+		{
+			Pickupable pickupable = item.item;
+			Transform transform = MainCameraControl.main.transform;
+			Vector3 dropPosition = Inventory.RayCast(transform.position, transform.forward, 10f, 0.75f, 1.5f);
+			pickupable.Drop(dropPosition);
+			if (pickupable.randomizeRotationWhenDropped)
+			{
+				pickupable.transform.rotation = Random.rotation;
+			}
+			SkyEnvironmentChanged.Send(pickupable.gameObject, Player.main.GetSkyEnvironment());
 		}
 	}
 }
